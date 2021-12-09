0d82995afa4a29083b291237c88c127c39718be1
Hojun_Yoo <lisiee0@naver.com>
2021-12-09 10:29:09 +0900

    ex03

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -9,14 +9,15 @@ public class GoodsApp {
     public static void main(String[] args) {
     	
     	List<Goods> gList= new ArrayList<Goods>();
-    	String Info, name;
+    	String Info = "", name;
     	int price, count, sum= 0;
 	
 		Scanner sc= new Scanner(System.in);
 		System.out.println("상품을 입력해주세요 (종료 q)");
 		
-    	try {	
-			do {
+	
+		do {
+			try {
 				Info= sc.nextLine();
 				
 				if(Info.equals("q")) {
@@ -24,24 +25,28 @@ public class GoodsApp {
 					System.out.println("==================================");
 					break;
 				}
+				
 				name= Info.split(",")[0];
 				price= Integer.parseInt(Info.split(",")[1]);
 				count= Integer.parseInt(Info.split(",")[2]);
 				
 				Goods newgoods= new Goods(name, price, count);
 				gList.add(newgoods);
+				
 				sum+= count;
-	    	}
-			while (Info!="q");
-
-			for(Goods g: gList) {
-				g.stats();
 			}
-			System.out.println("모든 상품의 갯수는 "+sum+"개 입니다.");
-			sc.close();
-    	}		
-		catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("유효하지 않은 명령어입니다.");
-		}  
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("유효하지 않은 명령어입니다.");
+			}
+
+    	}
+		while (Info!="q");
+
+		for(Goods g: gList) {
+			g.stats();
+		}
+		System.out.println("모든 상품의 갯수는 "+sum+"개 입니다.");
+		sc.close();		
+  
     }
 }
