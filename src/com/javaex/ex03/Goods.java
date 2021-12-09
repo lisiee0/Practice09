d68a620ec1b46613b92c1eac56dde254fcf94bb3
Hojun_Yoo <lisiee0@naver.com>
2021-12-09 09:50:09 +0900

    ex03

--- src/com/javaex/ex03/Goods.java
+++ src/com/javaex/ex03/Goods.java
@@ -5,7 +5,15 @@ public class Goods {
 	private String name;
 	private int price;
 	private int count;
-
 	
-
+	
+	public Goods(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+	
+	public void stats() {
+		System.out.println(name+" (가격: "+price+"원)이 "+count+"개 입고 되었습니다.");
+	}
 }
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,10 +1,47 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
     public static void main(String[] args) {
+    	
+    	List<Goods> gList= new ArrayList<Goods>();
+    	String Info, name;
+    	int price, count, sum= 0;
+	
+		Scanner sc= new Scanner(System.in);
+		System.out.println("상품을 입력해주세요 (종료 q)");
+		
+    	try {	
+			do {
+				Info= sc.nextLine();
+				
+				if(Info.equals("q")) {
+					System.out.println("[입력완료]");
+					System.out.println("==================================");
+					break;
+				}
+				name= Info.split(",")[0];
+				price= Integer.parseInt(Info.split(",")[1]);
+				count= Integer.parseInt(Info.split(",")[2]);
+				
+				Goods newgoods= new Goods(name, price, count);
+				gList.add(newgoods);
+				sum+= count;
+	    	}
+			while (Info!="q");
 
-       
+			for(Goods g: gList) {
+				g.stats();
+			}
+			System.out.println("모든 상품의 갯수는 "+sum+"개 입니다.");
+			sc.close();
+    	}		
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("유효하지 않은 명령어입니다.");
+			}  
     }
-
 }
